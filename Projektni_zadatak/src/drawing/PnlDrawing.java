61530fa7ff923682a7f4fc2c1962dc17898b0f63
Ivan Stanković <stankovic.it22.2020@uns.ac.rs>
2024-09-21 16:28:19 +0200

    removeSelected
    Takodje, u proslom commitu sam zaboravio da dodam, dodata je i metoda
    removeSelected() koja ce kasnije sluziti za brisanje elemenata pomocu
    dugmeta Delete.

--- Projektni_zadatak/src/drawing/PnlDrawing.java
+++ Projektni_zadatak/src/drawing/PnlDrawing.java
@@ -60,7 +60,7 @@ public class PnlDrawing extends JPanel {
 		shapes.set(index, shape);
 	}
 	
-	public void removeSelected() {
+	public void removeSelected() { //prosli commit
 	    for (int i = shapes.size() - 1; i >= 0; i--) {
 	        if (shapes.get(i).isSelected()) {
 	            shapes.remove(i);
